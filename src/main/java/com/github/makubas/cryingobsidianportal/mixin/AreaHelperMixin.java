b0f38c933158c93b9181628bbba4ff383f965ec4
Jakub Westa <jakub.westa@gmail.com>
2021-06-17 21:00:26 +0200

    Added README.md

--- src/main/java/com/github/makubas/cryingobsidianportal/mixin/AreaHelperMixin.java
+++ src/main/java/com/github/makubas/cryingobsidianportal/mixin/AreaHelperMixin.java
@@ -5,10 +5,12 @@ import net.minecraft.block.AbstractBlock;
 import net.minecraft.block.Blocks;
 import net.minecraft.world.dimension.AreaHelper;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.gen.Accessor;
+import org.spongepowered.asm.mixin.gen.Invoker;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
 
 @Mixin(AreaHelper.class)
 public class AreaHelperMixin {
-    private static final AbstractBlock.ContextPredicate IS_VALID_FRAME_BLOCK = (state, world, pos) -> {
-        return (state.isOf(Blocks.OBSIDIAN) || state.isOf(Blocks.CRYING_OBSIDIAN));
-    };
+    private static final AbstractBlock.ContextPredicate IS_VALID_FRAME_BLOCK = (state, world, pos) -> (state.isOf(Blocks.OBSIDIAN) || state.isOf(Blocks.CRYING_OBSIDIAN));
 }
